e5588d022825a692a93cd381ad339539aa890d60
bzmay0149 <mayjuanbaticono@gmail.com>
2023-07-28 10:12:53 +0200

    ultimo commit

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
-# from flask import Flask, jsonify
-# from models.queries import get_all_preguntas
-# from flask_cors import CORS
-# app = Flask(__name__)
-# CORS(app)
-# # Ruta para obtener todas las preguntas
-# @app.route('/Preguntas', methods=['GET'])
-# def obtener_preguntas():
-#     preguntas = get_all_preguntas()
-#     return jsonify(preguntas)
-
-# # Aquí puedes definir más rutas para obtener respuestas, temarios, niveles, etc.
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 
 from flask import Flask, jsonify
-from models.queries import get_all_preguntas
+from models.queries import get_all_preguntas, get_preguntas_html_inicio, get_preguntas_html_medio, get_preguntas_html_experto
+
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 
 # Ruta para obtener todas las preguntas
 @app.route('/Preguntas', methods=['GET'])
@@ -28,6 +15,25 @@ def obtener_preguntas():
     
     preguntas = get_all_preguntas()
     return jsonify(preguntas)
+
+# Nueva ruta para obtener preguntas de HTML Inicial
+@app.route('/Preguntas/html/inicio', methods=['GET'])
+def obtener_preguntas_html_inicio():
+    preguntas_html_inicio = get_preguntas_html_inicio()
+    return jsonify(preguntas_html_inicio)
+
+
+# Nueva ruta para obtener preguntas de HTML medio
+@app.route('/Preguntas/html/medio', methods=['GET'])
+def obtener_preguntas_html_medio():
+    preguntas_html_medio = get_preguntas_html_medio()
+    return jsonify(preguntas_html_medio)
+
+# Nueva ruta para obtener preguntas de HTML experto
+@app.route('/Preguntas/html/experto', methods=['GET'])
+def obtener_preguntas_html_experto():
+    preguntas_html_experto = get_preguntas_html_experto()
+    return jsonify(preguntas_html_experto)
 
 # Aquí puedes definir más rutas para obtener respuestas, temarios, niveles, etc.
 
--- models/queries.py
+++ models/queries.py
@@ -1,60 +1,18 @@
-# from database.connect_db import create_connection
 
-# def get_all_preguntas():
-#     try:
-#         connection = create_connection()
-#         cursor = connection.cursor()
-
-#         query = 'SELECT p.idpregunta, p.pregunta, p.idtema, p.idnivel, r.respuesta, r.correcta FROM Preguntas p JOIN Respuestas r ON p.idpregunta = r.idpregunta'
-#         cursor.execute(query)
-
-#         preguntas = []
-#         for row in cursor.fetchall():
-#             idpregunta = row[0]
-#             pregunta = row[1]
-#             idtema = row[2]
-#             idnivel = row[3]
-#             respuesta = row[4]
-#             correcta = row[5]
-
-#             # Buscar la pregunta en la lista de preguntas
-#             pregunta_actual = next((p for p in preguntas if p['idpregunta'] == idpregunta), None)
-
-#             # Si la pregunta no existe en la lista, agregarla
-#             if not pregunta_actual:
-#                 pregunta_actual = {
-#                     'idpregunta': idpregunta,
-#                     'pregunta': pregunta,
-#                     'idtema': idtema,
-#                     'idnivel': idnivel,
-#                     'respuestas': [],
-#                     'respuestaCorrecta': None
-#                 }
-#                 preguntas.append(pregunta_actual)
-
-#             # Agregar la respuesta a la lista de respuestas de la pregunta
-#             pregunta_actual['respuestas'].append(respuesta)
-
-#             # Si la respuesta es correcta, asignarla a la propiedad 'respuestaCorrecta'
-#             if correcta:
-#                 pregunta_actual['respuestaCorrecta'] = respuesta
-
-#         cursor.close()
-#         connection.close()
-
-#         return preguntas
-
-#     except Exception as e:
-#         return {'error': str(e)}
 
 from database.connect_db import create_connection
 
+# Función para obtener todas las preguntas
 def get_all_preguntas():
     try:
         connection = create_connection()
         cursor = connection.cursor()
 
-        query = 'SELECT p.idpregunta, p.pregunta, p.idtema, p.idnivel, r.respuesta, r.correcta FROM Preguntas p JOIN Respuestas r ON p.idpregunta = r.idpregunta'
+        query = '''
+            SELECT p.idpregunta, p.pregunta, p.idtema, p.idnivel, r.respuesta, r.correcta 
+            FROM Preguntas p 
+            JOIN Respuestas r ON p.idpregunta = r.idpregunta
+        '''
         cursor.execute(query)
 
         preguntas = []
@@ -94,7 +52,169 @@ def get_all_preguntas():
         return preguntas
 
     except Exception as e:
-        return {'error': str(e)}      
+        return {'error': str(e)}
+
+# Función para obtener preguntas de "HTML Inicial"
+def get_preguntas_html_inicio():
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        query = '''
+             SELECT p.idpregunta, p.pregunta, p.idtema, p.idnivel, r.respuesta, r.correcta 
+    FROM Preguntas p 
+    JOIN Respuestas r ON p.idpregunta = r.idpregunta 
+    WHERE p.idnivel = 2001 AND p.idtema = 3001
+        '''
+        cursor.execute(query)
+
+        preguntas = []
+        for row in cursor.fetchall():
+            idpregunta = row[0]
+            pregunta = row[1]
+            idtema = row[2]
+            idnivel = row[3]
+            respuesta = row[4]
+            correcta = row[5]
+
+            # Buscar la pregunta en la lista de preguntas
+            pregunta_actual = next((p for p in preguntas if p['idpregunta'] == idpregunta), None)
+
+            # Si la pregunta no existe en la lista, agregarla
+            if not pregunta_actual:
+                pregunta_actual = {
+                    'idpregunta': idpregunta,
+                    'pregunta': pregunta,
+                    'idtema': idtema,
+                    'idnivel': idnivel,
+                    'respuestas': [],
+                    'respuestaCorrecta': None
+                }
+                preguntas.append(pregunta_actual)
+
+            # Agregar la respuesta a la lista de respuestas de la pregunta
+            pregunta_actual['respuestas'].append(respuesta)
+
+            # Si la respuesta es correcta, asignarla a la propiedad 'respuestaCorrecta'
+            if correcta:
+                pregunta_actual['respuestaCorrecta'] = respuesta
+
+        cursor.close()
+        connection.close()
+
+        return preguntas
+
+    except Exception as e:
+        return {'error': str(e)}
+    
+    # Función para obtener preguntas de "HTML medio"
+def get_preguntas_html_medio():
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        query = '''
+             SELECT p.idpregunta, p.pregunta, p.idtema, p.idnivel, r.respuesta, r.correcta 
+    FROM Preguntas p 
+    JOIN Respuestas r ON p.idpregunta = r.idpregunta 
+    WHERE p.idnivel = 2002 AND p.idtema = 3001
+        '''
+        cursor.execute(query)
+
+        preguntas = []
+        for row in cursor.fetchall():
+            idpregunta = row[0]
+            pregunta = row[1]
+            idtema = row[2]
+            idnivel = row[3]
+            respuesta = row[4]
+            correcta = row[5]
+
+            # Buscar la pregunta en la lista de preguntas
+            pregunta_actual = next((p for p in preguntas if p['idpregunta'] == idpregunta), None)
+
+            # Si la pregunta no existe en la lista, agregarla
+            if not pregunta_actual:
+                pregunta_actual = {
+                    'idpregunta': idpregunta,
+                    'pregunta': pregunta,
+                    'idtema': idtema,
+                    'idnivel': idnivel,
+                    'respuestas': [],
+                    'respuestaCorrecta': None
+                }
+                preguntas.append(pregunta_actual)
+
+            # Agregar la respuesta a la lista de respuestas de la pregunta
+            pregunta_actual['respuestas'].append(respuesta)
+
+            # Si la respuesta es correcta, asignarla a la propiedad 'respuestaCorrecta'
+            if correcta:
+                pregunta_actual['respuestaCorrecta'] = respuesta
+
+        cursor.close()
+        connection.close()
+
+        return preguntas
+
+    except Exception as e:
+        return {'error': str(e)}
+    
+    # Función para obtener preguntas de "HTML experto"
+def get_preguntas_html_experto():
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        query = '''
+             SELECT p.idpregunta, p.pregunta, p.idtema, p.idnivel, r.respuesta, r.correcta 
+    FROM Preguntas p 
+    JOIN Respuestas r ON p.idpregunta = r.idpregunta 
+    WHERE p.idnivel = 2003 AND p.idtema = 3001
+        '''
+        cursor.execute(query)
+
+        preguntas = []
+        for row in cursor.fetchall():
+            idpregunta = row[0]
+            pregunta = row[1]
+            idtema = row[2]
+            idnivel = row[3]
+            respuesta = row[4]
+            correcta = row[5]
+
+            # Buscar la pregunta en la lista de preguntas
+            pregunta_actual = next((p for p in preguntas if p['idpregunta'] == idpregunta), None)
+
+            # Si la pregunta no existe en la lista, agregarla
+            if not pregunta_actual:
+                pregunta_actual = {
+                    'idpregunta': idpregunta,
+                    'pregunta': pregunta,
+                    'idtema': idtema,
+                    'idnivel': idnivel,
+                    'respuestas': [],
+                    'respuestaCorrecta': None
+                }
+                preguntas.append(pregunta_actual)
+
+            # Agregar la respuesta a la lista de respuestas de la pregunta
+            pregunta_actual['respuestas'].append(respuesta)
+
+            # Si la respuesta es correcta, asignarla a la propiedad 'respuestaCorrecta'
+            if correcta:
+                pregunta_actual['respuestaCorrecta'] = respuesta
+
+        cursor.close()
+        connection.close()
+
+        return preguntas
+
+    except Exception as e:
+        return {'error': str(e)}
+   
+   
+   
 
 
 
